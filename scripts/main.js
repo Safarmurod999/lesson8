6e92d2c05fab8579783cee3058828ee7db005624
Safarmurod999 <supermen0904@gmail.com>
2022-12-11 00:20:59 +0500

    first commmit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.left = "0";
+    // document.getElementById("navbar-menus").style.top = "0";
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.left = "-100%";
+    // document.getElementById("navbar-menus").style.top = "-1000%";
+  }
